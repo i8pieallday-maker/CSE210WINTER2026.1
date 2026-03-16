25a821bddf5d3aaff9a7b7525b3230d0c568a9e7
i8pieallday-maker <i8pieallday@gmail.com>
2026-03-16 14:23:29 -0600

    done

--- prove/Develop03/ScriptureProgram.cs
+++ prove/Develop03/ScriptureProgram.cs
@@ -12,9 +12,14 @@ class Program
     {
 
         List<Scripture> scriptures = new List<Scripture>();
-        scriptures.Add(new Scripture("Alma", 5, 1, 2, "and my father dwelt in a tent"));
-        scriptures.Add(new Scripture("Helaman", 5, 12, 0, "And now my sons, I say unto you, Fear not. "))
+        //scriptures.Add(new Scripture("Alma", 5, 1, 2, "and my father dwelt in a tent"));
+        scriptures.Add(new Scripture("Helaman", 5, 12, 0, "And now my sons, I say unto you, Fear not. "));
         scriptures.Add(new Scripture("Alma", 7, 11, 12, "Took upon Him death."));
+        scriptures.Add(new Scripture("Jeremiah", 33, 3, 0, "Call unto me, and I will answer thee, and show thee great and mighty things, which thou knowest not."));
+        scriptures.Add(new Scripture("Matthew", 7, 7, 0, "Ask, and it shall be given you; seek, and ye shall find; knock, and it shall be opened unto you."));
+        scriptures.Add(new Scripture("Philippians", 4, 13, 0, "I can do all things through Christ, which strengtheneth me."));
+        scriptures.Add(new Scripture("Matthew", 6, 33, 0, "But seek ye first the kingdom of God, and his righteousness; and all these things shall be added unto you"));
+        scriptures.Add(new Scripture("James", 4, 7, 0, "Submit yourselves therefore to God. Resist the devil, and he will flee from you."));
         Random rand = new Random();
         Scripture scripture = scriptures[rand.Next(scriptures.Count)];
         Display(scripture);
